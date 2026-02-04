72893bb6f38f99ae6bc6c3cecce80cd846cb1942
Piyush Kulkarni <kulkarnipiyush462@gmail.com>
2026-02-04 12:11:18 +0530

    pdf

---
+++ synthetic_data/data_genration.py
@@ -0,0 +1,13 @@
+"""
+Schema:
+Incident ID []
+Date [ANY DATE IN 2025]
+Location : ["Brahmanvel Wind Farm", "Dhalgaon Wind Farm", "Vankusawade Wind Farm", "Vaspet Wind Farm", "Tuljapur Wind Farm", "Andhra Lake Wind Farm", "Koral Wind Farm", "Jath Wind Farm"]
+Turbine ID []
+Observed Issue
+Root Cause
+Corrective Action
+Downtime Impact
+
+"""
+
